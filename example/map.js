12a9ae6da0ebe57e3204ebbac96c533423e41d3c
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-07-12 22:51:42 -1000

    demo loading from real tiles

--- example/map.js
+++ example/map.js
@@ -1,6 +1,7 @@
 var mixmap = require('../')
 var regl = require('regl')
 var glsl = require('glslify')
+var resl = require('resl')
 
 var mix = mixmap(regl, { extensions: ['oes_element_index_uint'] })
 var map = mix.create()
@@ -11,11 +12,15 @@ var drawTile = map.createDraw({
     precision highp float;
     #pragma glslify: hsl2rgb = require('glsl-hsl2rgb')
     uniform float id;
+    uniform sampler2D texture;
+    varying vec2 vtcoord;
     void main () {
       float h = mod(id/8.0,1.0);
       float s = mod(id/4.0,1.0)*0.5+0.25;
       float l = mod(id/16.0,1.0)*0.5+0.25;
-      gl_FragColor = vec4(hsl2rgb(h,s,l),1);
+      vec3 c = hsl2rgb(h,s,l);
+      vec4 tc = texture2D(texture,vtcoord);
+      gl_FragColor = vec4(c*(1.0-tc.a)+tc.rgb*tc.a,1);
     }
   `,
   vert: `
@@ -23,8 +28,11 @@ var drawTile = map.createDraw({
     attribute vec2 position;
     uniform vec4 viewbox;
     uniform vec2 offset;
+    attribute vec2 tcoord;
+    varying vec2 vtcoord;
     void main () {
       vec2 p = position + offset;
+      vtcoord = tcoord;
       gl_Position = vec4(
         (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
         (p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0,
@@ -32,27 +40,42 @@ var drawTile = map.createDraw({
     }
   `,
   uniforms: {
-    id: map.prop('id')
+    id: map.prop('id'),
+    texture: map.prop('texture')
   },
   attributes: {
-    position: map.prop('points')
+    position: map.prop('points'),
+    tcoord: [0,1,0,0,1,1,1,0] // sw,se,nw,ne
   },
   elements: [0,1,2,1,2,3]
 })
+
 map.addLayer({
   viewbox: function (bbox, zoom, cb) {
     cb(null, tiles)
   },
   add: function (key, bbox) {
     var file = '50m/' + bbox.join('x') + '.jpg'
-    drawTile.props.push({
+    var prop = {
       id: Number(key),
+      texture: map.regl.texture(),
       points: [
-        bbox[0], bbox[1],
-        bbox[0], bbox[3],
-        bbox[2], bbox[1],
-        bbox[2], bbox[3]
+        bbox[0], bbox[1], // sw
+        bbox[0], bbox[3], // se
+        bbox[2], bbox[1], // nw
+        bbox[2], bbox[3]  // ne
       ]
+    }
+    drawTile.props.push(prop)
+    map.draw()
+    resl({
+      manifest: { tile: { type: 'image', src: file } },
+      onDone: function (assets) {
+        window.requestIdleCallback(function () {
+          prop.texture = map.regl.texture(assets.tile)
+          map.draw()
+        })
+      }
     })
   },
   remove: function (key, bbox) {
@@ -93,7 +116,7 @@ app.route('/cool', function (state, emit) {
 app.route('*', function (state, emit) {
   return html`<body>
     ${mix.render()}
-    <h1>mixmap</h1>
+    <h1>mixmap ${state.now}</h1>
     <a href="/cool">cool</a>
     <div>
       <button onclick=${zoomIn}>zoom in</button>
